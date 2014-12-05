32d1eebc5999c4d22f26b9a0ad57f6bdb8d50929
Brandon Bayer <feedthebayer@users.noreply.github.com>
2014-12-04 21:27:47 -0600

    refactor: clean up paginate method

--- lib/paginate.rb
+++ lib/paginate.rb
@@ -1,13 +1,9 @@
 module Paginate
 
   def paginate(input = { page: 1, per_page: 10 } )
-    # Ensure :page != nil
-    input[:page] ||= 1
-
-    # page = input[:page]
-    # multiplier = page - 1
-    # offset = per_page * multiplier
-    # limit(per_page).offset(offset)
-    limit(input[:per_page]).offset(input[:per_page] * (input[:page] - 1))
+    page     = input[:page] || 1
+    per_page = input[:per_page]
+    offset   = per_page * (page - 1)
+    limit(per_page).offset(offset)
   end
 end
