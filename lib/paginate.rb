24f4bd7a64e80d2cf2465e222b2388ad990e95f1
Brandon Bayer <feedthebayer@users.noreply.github.com>
2014-12-04 21:33:48 -0600

    style: fix formating

--- lib/paginate.rb
+++ lib/paginate.rb
@@ -1,6 +1,5 @@
 module Paginate
-
-  def paginate(input = { page: 1, per_page: 10 } )
+  def paginate(input = { page: 1, per_page: 10 })
     page     = input[:page] || 1
     per_page = input[:per_page]
     offset   = per_page * (page - 1)
