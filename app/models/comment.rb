db08063ebcb1063758a5af04f7f1b6e0890e7d2a
Brandon Bayer <feedthebayer@users.noreply.github.com>
2014-12-06 17:58:50 -0600

    feat: add comment show and new to post pages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 5 }
   validates :user, presence: true
 end
