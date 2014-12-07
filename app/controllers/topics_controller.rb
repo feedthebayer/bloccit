47f556876478db34d7ab182d78a7bfb9d36c3299
Brandon Bayer <feedthebayer@users.noreply.github.com>
2014-12-07 12:37:40 -0600

    feat: add ability to destroy topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -42,6 +42,19 @@ class TopicsController < ApplicationController
     end
   end
 
+  def destroy
+    @topic = Topic.find(params[:id])
+    name = @topic.name
+    authorize @topic
+
+    if @topic.destroy
+      redirect_to topics_path, notice: "\"#{name}\" was deleted."
+    else
+      flash[:error] = "Error deleting the topic."
+      render :show
+    end
+  end
+
   private
 
   def topic_params
