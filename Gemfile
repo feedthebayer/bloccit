b3e605c574a31d2374aa3fe9d27dd9d46892ab41
Brandon Bayer <feedthebayer@users.noreply.github.com>
2014-12-10 20:24:34 -0600

    feat: install rspec

--- Gemfile
+++ Gemfile
@@ -11,6 +11,10 @@ group :development do
   gem 'sqlite3'
 end
 
+group :test do
+  gem 'rspec-rails', '~> 3.1.0'
+end
+
 gem 'rails', '4.1.6'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Vote do
+  describe "validations" do
+    describe "value validation"
+    it "only allows -1 or 1 as values" do
+      v = Vote.new(value: 1)
+      expect(v.valid?).to eq(true)
+
+      v = Vote.new(value: -1)
+      expect(v.valid?).to eq(true)
+
+      v = Vote.new(value: 0)
+      expect(v.valid?).to eq(false)
+
+      v = Vote.new(value: 2)
+      expect(v.valid?).to eq(false)
+
+      v = Vote.new(value: -2)
+      expect(v.valid?).to eq(false)
+    end
+  end
+end
