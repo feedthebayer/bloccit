57eeed7d6846e1f805563b7adcdefb4fdd689633
Brandon Bayer <feedthebayer@users.noreply.github.com>
2014-12-13 11:43:05 -0600

    feat: complete votes controller

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,12 @@ class Vote < ActiveRecord::Base
   belongs_to :post
   validates :value, inclusion: { in: [-1, 1],
                                  message: "%{value} is not a valid vote." }
+
+  def max?
+    value == 1
+  end
+
+  def min?
+    value == -1
+  end
 end
