ff11368120eaeb19958a5bf8dc51ae0aad3a3b21
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-01-06 20:24:05 -0600

    test: add vote controller test and refactor helper methods

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Post do
+  include TestFactories
+
   describe "vote methods" do
 
     before do
@@ -38,23 +40,3 @@ describe Post do
   end
 end
 
-def associated_post(options={})
-  post_options = {
-    title: 'Post title',
-    body:  'Post bodies must be long',
-    topic: Topic.create(name: 'Topic name'),
-    user:  authenticated_user
-  }.merge(options)
-  Post.create(post_options)
-end
-
-def authenticated_user(options={})
-  user_options = {
-    email: "email#{rand}@example.com",
-    password: 'password'
-  }.merge(options)
-  user = User.new(user_options)
-  user.skip_confirmation!
-  user.save
-  user
-end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,23 +1,36 @@
 require 'rails_helper'
 
 describe Vote do
+  include TestFactories
+
   describe "validations" do
-    describe "value validation"
-    it "only allows -1 or 1 as values" do
-      v = Vote.new(value: 1)
-      expect(v.valid?).to eq(true)
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        v = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
 
-      v = Vote.new(value: -1)
-      expect(v.valid?).to eq(true)
+        v = Vote.new(value: -1)
+        expect(v.valid?).to eq(true)
 
-      v = Vote.new(value: 0)
-      expect(v.valid?).to eq(false)
+        v = Vote.new(value: 0)
+        expect(v.valid?).to eq(false)
 
-      v = Vote.new(value: 2)
-      expect(v.valid?).to eq(false)
+        v = Vote.new(value: 2)
+        expect(v.valid?).to eq(false)
 
-      v = Vote.new(value: -2)
-      expect(v.valid?).to eq(false)
+        v = Vote.new(value: -2)
+        expect(v.valid?).to eq(false)
+      end
+    end
+  end
+
+  describe "after_save" do
+    it "calls 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
     end
   end
 end
+
