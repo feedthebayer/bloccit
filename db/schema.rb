adb95af3c92ad8516e38311d8a913786dd4ac339
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-01-12 20:54:47 -0600

    feat: add user preference for receiving update emails [TEST PASSING BUT NOT CORRECT]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111155525) do
+ActiveRecord::Schema.define(version: 20150113021509) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -58,12 +58,12 @@ ActiveRecord::Schema.define(version: 20150111155525) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20150111155525) do
     t.datetime "updated_at"
     t.string   "role"
     t.string   "avatar"
-    t.boolean  "email_favorites",        default: false
+    t.boolean  "email_favorites",        default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
