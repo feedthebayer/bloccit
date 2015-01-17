baa3bae8e6fbc5f27f706d38a9a3650337b46bb6
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-01-17 14:53:44 -0600

    test: add test for signed in user visiting user profile

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -6,12 +6,26 @@ describe "Visiting profiles" do
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A comment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save
+    @comment = comment_without_email(user: @user, post_id: @post.id)
   end
 
   describe "not signed in" do
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
+  end
+
+  describe "signed in" do
+    include Warden::Test::Helpers
+    Warden.test_mode!
+
+    before { login_as @user, scope: :user }
+
     it "shows profile" do
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
