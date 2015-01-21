90586a3c4fbd132c1eaa0f19ba7702cd944a74db
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-01-20 20:42:59 -0600

    test: add user_with_post_and_comment factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,15 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+
+    factory :user_with_post_and_comment do
+      transient do
+        count 1
+      end
+      after(:build) do |user, evalulator|
+        create_list(:post, evalulator.count, user: user)
+        create_list(:comment, evalulator.count, user: user)
+      end
+    end
   end
 end
